de8c2e0e83bba8b39a97b5c162d73f0888ff763c
Brian Auton <brianauton@gmail.com>
2014-06-24 14:08:48 -0400

    Skip mass-assignment protection when creating jobs

--- lib/bj/table.rb
+++ lib/bj/table.rb
@@ -133,7 +133,7 @@ class Bj
           transaction do
             jobs.each do |job|
               job = create_hash_for(job.reverse_merge(submit_defaults))
-              job = create! job 
+              job = create! job, without_protection: true
               returned << (block ? block.call(job) : job)
             end
           end
